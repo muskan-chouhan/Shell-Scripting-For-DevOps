38d8fc60150ccc73b669cf4a8dad544fac6248c4
muskan-chouhan <mushkanc430@gmail.com>
2026-09-06 18:53:11 +0530

    Add function.sh

---
+++ day-05/function.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+greet(){
+  echo "Hello, $1! Welcome to Bash scripting."
+}
+greet "muskan"
+greet "DevOps Student"                                                                                  7,1     
